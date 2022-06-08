5e049ba9923fe2a7b849fdbe2481604f09bc0343
Henk Keizer <hwkeizer@telfort.nl>
2022-06-08 17:20:47 +0200

    Test commit

--- src/main/java/com/example/demo/TestClass.java
+++ src/main/java/com/example/demo/TestClass.java
@@ -11,7 +11,7 @@ public class TestClass {
 	BuildProperties buildProperties;
 	
 	public TestClass() {
-		System.out.println(buildProperties.getVersion());
+		System.out.println(buildProperties);
 	}
 	
 }
